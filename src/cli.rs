1f0eea9ddceaf864ff13f2104e1b215860d595d6
Joseph J. Bautista <joseph.bautista@proton.me>
2024-12-17 22:32:14 +0100

    refactor: move cmd to separate functions

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,8 @@
-use clap::{command, Arg, Command};
+use serde_json;
+use clap::{command, Arg, Command, ArgMatches};
+
+use crate::alpaca_api::AlpacaClient;
+use crate::credentials::{write_credentials, Credentials};
 
 pub fn get_cli_matches() -> clap::ArgMatches {
   command!()
@@ -38,4 +42,55 @@ pub fn get_cli_matches() -> clap::ArgMatches {
     )
     .subcommand(Command::new("positions"))
     .get_matches()
+}
+
+pub fn handle_prices_cmd(prices_args_opt: Option<&ArgMatches>, api_key: String, api_secret: String) {
+  let price_args = prices_args_opt.unwrap();
+    let symbol = price_args.get_one::<String>("symbol")
+      .unwrap_or(&"NONE".to_string())
+      .to_uppercase();
+
+    let client = AlpacaClient::new(api_key, api_secret);
+    match client.fetch_asset(&symbol) {
+      Ok(json) => println!("{}", serde_json::to_string_pretty(&json).unwrap()),
+      Err(e) => {
+        eprintln!("Error fetching asset details: {}", e);
+        std::process::exit(1);
+      }
+    }
+}
+
+pub fn handle_auth_cmd(auth_args: &ArgMatches) {
+  let mut new_credentials = Credentials {
+    apca_api_key: "".to_string(),
+    apca_secret_key: "".to_string()
+  };
+
+  let auth_set_opts = auth_args.subcommand_matches("set");
+  if auth_set_opts.is_some() {
+    let auth_set_args = auth_set_opts.unwrap();
+
+    let empty_string = "".to_string();
+    let apca_api_key = auth_set_args.get_one::<String>("api-key")
+      .unwrap_or(&empty_string)
+      .to_string();
+    let apca_secret_key = auth_set_args.get_one::<String>("secret-key")
+      .unwrap_or(&empty_string)
+      .to_string();
+
+    new_credentials.apca_api_key = apca_api_key;
+    new_credentials.apca_secret_key = apca_secret_key;
+
+    if let Err(e) = write_credentials(&new_credentials) {
+      eprintln!("Failed to write credentials: {}", e);
+      std::process::exit(1);
+    }
+  }
+
+  if let Some(_) = auth_args.subcommand_matches("reset") {
+    if let Err(e) = write_credentials(&new_credentials) {
+      eprintln!("Failed to write credentials: {}", e);
+      std::process::exit(1);
+    }
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
-use serde_json;
-
-mod cli;
-use cli::get_cli_matches;
-
-mod api;
-use api::AlpacaClient;
-
+mod alpaca_api;
 mod credentials;
-use credentials::{Credentials, read_credentials, write_credentials};
+mod cli;
+
+use cli::{get_cli_matches, handle_prices_cmd, handle_auth_cmd};
+use credentials::read_credentials;
 
 fn main() {
   let match_result = get_cli_matches();
@@ -22,58 +18,13 @@ fn main() {
   let api_key = credentials.apca_api_key.to_string();
   let api_secret = credentials.apca_secret_key.to_string();
 
-  // Shorter way: if let Some(price_args) = match_result.subcommand_matches("prices") {
   let prices_args_opt = match_result.subcommand_matches("prices");
   if prices_args_opt.is_some() {
-    let price_args = prices_args_opt.unwrap();
-    let symbol = price_args.get_one::<String>("symbol")
-      .unwrap_or(&"NONE".to_string())
-      .to_uppercase();
-
-    let client = AlpacaClient::new(api_key, api_secret);
-    match client.fetch_asset(&symbol) {
-      Ok(json) => println!("{}", serde_json::to_string_pretty(&json).unwrap()),
-      Err(e) => {
-        eprintln!("Error fetching asset details: {}", e);
-        std::process::exit(1);
-      }
-    }
+    handle_prices_cmd(prices_args_opt, api_key, api_secret);
   }
 
-  let auth_args_opts = match_result.subcommand_matches("auth");
-  if auth_args_opts.is_some() {
-    let auth_args = auth_args_opts.unwrap();
-    let mut new_credentials = Credentials {
-      apca_api_key: "".to_string(),
-      apca_secret_key: "".to_string()
-    };
-
-    let auth_set_opts = auth_args.subcommand_matches("set");
-    if auth_set_opts.is_some() {
-      let auth_set_args = auth_set_opts.unwrap();
-
-      let empty_string = "".to_string();
-      let apca_api_key = auth_set_args.get_one::<String>("api-key")
-        .unwrap_or(&empty_string)
-        .to_string();
-      let apca_secret_key = auth_set_args.get_one::<String>("secret-key")
-        .unwrap_or(&empty_string)
-        .to_string();
-
-      new_credentials.apca_api_key = apca_api_key;
-      new_credentials.apca_secret_key = apca_secret_key;
-
-      if let Err(e) = write_credentials(&new_credentials) {
-        eprintln!("Failed to write credentials: {}", e);
-        std::process::exit(1);
-      }
-    }
-
-    if let Some(_) = auth_args.subcommand_matches("reset") {
-      if let Err(e) = write_credentials(&new_credentials) {
-        eprintln!("Failed to write credentials: {}", e);
-        std::process::exit(1);
-      }
-    }
+  // Writing this verbosely for learning purposes - J
+  if let Some(auth_args) = match_result.subcommand_matches("auth") {
+    handle_auth_cmd(auth_args);
   }
 }
