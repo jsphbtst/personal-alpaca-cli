6acdcbee83d24f359f002a38ce7d4d6cc0973b26
Joseph J. Bautista <joseph.bautista@proton.me>
2024-12-17 23:02:22 +0100

    refactor: implement private base_request fn

--- src/alpaca_api.rs
+++ src/alpaca_api.rs
@@ -3,21 +3,22 @@ use reqwest::blocking::Client;
 pub struct AlpacaClient {
   api_key: String,
   api_secret: String,
+  base_url: String,
   client: Client,
 }
 
 impl AlpacaClient {
   pub fn new(api_key: String, api_secret: String) -> Self {
+    let url = "https://api.alpaca.markets";
     Self {
       api_key,
       api_secret,
+      base_url: url.to_string(),
       client: Client::new(),
     }
   }
 
-  pub fn fetch_asset(&self, symbol: &str) -> Result<serde_json::Value, reqwest::Error> {
-    let url = format!("https://api.alpaca.markets/v2/assets/{}", symbol);
-
+  fn base_request(&self, url: String) -> Result<serde_json::Value, reqwest::Error> {
     let response = self.client.get(url)
       .header("Accept", "application/json")
       .header("APCA-API-KEY-ID", &self.api_key)
@@ -26,31 +27,20 @@ impl AlpacaClient {
       .json()?;
 
     Ok(response)
+  }
+
+  pub fn fetch_asset(&self, symbol: &str) -> Result<serde_json::Value, reqwest::Error> {
+    let url = format!("{}/v2/assets/{}", &self.base_url, symbol);
+    self.base_request(url)
   }
 
   pub fn fetch_positions(&self) -> Result<serde_json::Value, reqwest::Error> {
-    let url = "https://api.alpaca.markets/v2/positions";
-
-    let response = self.client.get(url)
-      .header("Accept", "application/json")
-      .header("APCA-API-KEY-ID", &self.api_key)
-      .header("APCA-API-SECRET-KEY", &self.api_secret)
-      .send()?
-      .json()?;
-
-    Ok(response)
+    let url = format!("{}/v2/positions", &self.base_url);
+    self.base_request(url)
   }
 
   pub fn fetch_positions_by_symbol(&self, symbol: String) -> Result<serde_json::Value, reqwest::Error> {
-    let url = format!("https://api.alpaca.markets/v2/positions/{}", symbol);
-
-    let response = self.client.get(url)
-      .header("Accept", "application/json")
-      .header("APCA-API-KEY-ID", &self.api_key)
-      .header("APCA-API-SECRET-KEY", &self.api_secret)
-      .send()?
-      .json()?;
-
-    Ok(response)
+    let url = format!("{}/v2/positions/{}", &self.base_url, symbol);
+    self.base_request(url)
   }
 }
